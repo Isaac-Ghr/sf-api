bf35e6749091e5673e128047db0bd183e2209d5f
cyb-3r <isaacgh830@gmail.com>
2024-03-03 22:35:13 +0100

    oops

--- src/Entity/Livre.php
+++ src/Entity/Livre.php
@@ -8,6 +8,8 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+// use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: LivreRepository::class)]
 class Livre
